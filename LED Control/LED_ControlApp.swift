7a25b35fe847c4a3d3515ebb11858bafadceb58e
Caden Kowalski <cadenkowalski@Cadens-MacBook-Pro.local>
2022-08-21 09:35:09 -0400

    Initialized Xcode project

---
+++ LED Control/LED_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  LED_ControlApp.swift
+//  LED Control
+//
+//  Created by Caden Kowalski on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct LED_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
